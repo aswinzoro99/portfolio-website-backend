4e1f9bbaacbae91a61ae2e59469ad7c1e17472c0
Aswin Ranjith <aswin.ranjith@fyers.in>
2026-03-28 23:08:51 +0530

    Added firebase impl for uploading urls

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,7 @@
 const API_BASE = '';
 
 /* === FIREBASE === */
-firebase.initializeApp({
-  apiKey: "AIzaSyCD8c_Pe_DfTGCHB0BsWY4XPMBDuw89FMQ",
-  authDomain: "akshay-ranjith-profolio.firebaseapp.com",
-  projectId: "akshay-ranjith-profolio",
-  storageBucket: "akshay-ranjith-profolio.firebasestorage.app",
-  messagingSenderId: "292316438815",
-  appId: "1:292316438815:web:f61cb6d518562f3fe8502a",
-  measurementId: "G-2YQV8TRGDE"
-});
+firebase.initializeApp(FIREBASE_CONFIG);
 firebase.analytics();
 
 /* === STATE === */
---
+++ public/js/firebase-config.js
@@ -0,0 +1,9 @@
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCD8c_Pe_DfTGCHB0BsWY4XPMBDuw89FMQ",
+  authDomain: "akshay-ranjith-profolio.firebaseapp.com",
+  projectId: "akshay-ranjith-profolio",
+  storageBucket: "akshay-ranjith-profolio.firebasestorage.app",
+  messagingSenderId: "292316438815",
+  appId: "1:292316438815:web:f61cb6d518562f3fe8502a",
+  measurementId: "G-2YQV8TRGDE"
+};
